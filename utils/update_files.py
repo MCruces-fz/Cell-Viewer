fc30658a9d6d083f046a4f4d643750901af8c270
MCruces-fz <mcsquared.fz@gmail.com>
2021-04-15 17:16:10 +0200

    Remove update_files.py

--- utils/update_files.py
+++
@@ -1,7 +0,0 @@
-from utils.const import *
-import os
-
-print("remember to update the directory DATA_DIR to your desired place")
-user = input("Type your Trucha user: ")
-DATA_DIR = ""
-os.system(f"scp -r {user}@fptrucha.usc.es:/home/labcaf/People/Pablo.Cabanelas/soft/png {DATA_DIR}/../. ")
