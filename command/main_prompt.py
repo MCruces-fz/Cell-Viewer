95ef0bfd66ba03ff1a14b01482140a2313b3b238
MCruces-fz <mcsquared.fz@gmail.com>
2021-04-23 13:49:44 +0200

    Add funcitonality to main.py
    Change theme and added documentation

--- command/main_prompt.py
+++ command/main_prompt.py
@@ -16,27 +16,50 @@ from cmd import Cmd
 
 
 class Prompt(Cmd):
+    prompt = '(~ºuº)~ '
+    intro = "\n"\
+            "Welcome!\n"\
+            "Type ? to list commands\n"
+
+    theme = "dark"
+
     def do_exit(self, inp):
-        print(f"Bye {inp}!")
+        print(f"Bye!")
+        print()
         return True
 
-    @staticmethod
-    def do_root(inp):
+    def help_exit(self):
+        print("Write 'exit' to close this.")
+        print()
+
+    def do_root(self, inp):
+        print("Launching ROOT reader User Graphical Interface...")
+
         from interface.gui_root import CellsAppROOT
         from kitchen.cook_root import CookDataROOT
         from utils.dirs import ROOT_DATA_DIR
 
         cook_data = CookDataROOT(data_dir=ROOT_DATA_DIR)
         # ary = cook_data.read_data()
-        CellsAppROOT(chef_object=cook_data, theme="dark")
+        CellsAppROOT(chef_object=cook_data, theme=self.theme)
 
         # TODO:
         #  Extraer todos los archivos ascii y guardarlos en:
         #  Datos4TB/tragaldabas/data/monitoring/cellmaps
         #  con nombres más cortitos y útiles
 
-    @staticmethod
-    def do_ascii(inp):
+        print("done")
+        print()
+
+    def help_root(self):
+        print("Check utils/dirs.py and:")
+        print("Add your path to root files in ROOT_DATA_DIR variable")
+        print("Then run: root")
+        print()
+
+    def do_ascii(self, inp):
+        print("Launching ASCII reader User Graphical Interface...")
+
         from interface.gui_ascii import CellsAppASCII
         from kitchen.cook_ascii import CookDataASCII
         from utils.dirs import ASCII_DATA_DIR
@@ -50,7 +73,27 @@ class Prompt(Cmd):
         #    - Poder escoger fecha y hora para el rango de análisis.
 
         cook_data = CookDataASCII(data_dir=ASCII_DATA_DIR)
-        CellsAppASCII(chef_object=cook_data)
+        CellsAppASCII(chef_object=cook_data, theme=self.theme)
 
-    def do_add(self, inp):
-        print(f"Adding \"{inp}\".")
+        print("done")
+        print()
+
+    def help_ascii(self):
+        print("Check utils/dirs.py and:")
+        print("Add your path to root files in ASCII_DATA_DIR variable")
+        print("Then run: ascii")
+        print()
+
+    def do_theme(self, theme):
+        if theme not in ["dark", "light"]:
+            print("Only dark and light themes available.")
+        self.theme = theme
+        print(f"--> \"{theme}\" theme set.")
+        print()
+
+    def help_theme(self):
+        print("Usage:")
+        print("theme dark: Use dark theme")
+        print("theme light: Use light theme")
+        print("Changes will be lost when you close this prompt")
+        print()
